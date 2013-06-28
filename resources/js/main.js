e842e7819c6d78fcb7a9498a51bbe41f61fb3cf0
Benjamin Cabé <BCabe@sierrawireless.com>
2013-06-28 16:53:45 +0200

    shuffle authors and improve anchors animation management

--- resources/js/main.js
+++ resources/js/main.js
@@ -26,7 +26,7 @@ $(function() {
                             $("body").append(div);
                             setTimeout(function() {
                                 div.remove();
-                                window.location.href = loc + "#" + split[1];
+                                window.location.hash = "#" + split[1];
                             }, 100);
                         }
                     });
@@ -35,5 +35,24 @@ $(function() {
             }
         }
     });
+    
+});
+
+
+$(function(){
+
+    $.fn.shuffle = function() {
+        return this.each(function(){
+            var items = $(this).children().clone(true);
+            return (items.length) ? $(this).html($.shuffle(items)) : this;
+        });
+    }
+    
+    $.shuffle = function(arr) {
+        for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+        return arr;
+    }
+
+     $('#authors').shuffle();
     
 });
